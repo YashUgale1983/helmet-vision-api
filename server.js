bcdc0e224394f2bc5c8eefd6497e0d0c57e6d39c
Yash Ugale <yashugale1983@gmail.com>
2024-04-21 15:20:01 +0530

    modified server.js to include labels in the response

--- server.js
+++ server.js
@@ -1,139 +1,67 @@
-// const express = require("express");
-// const fileUpload = require("express-fileupload");
-// const fs = require("fs");
-// const { ImageAnnotatorClient } = require("@google-cloud/vision");
-// const cors = require("cors");
-// require("dotenv").config();
-
-// const app = express();
-// const port = 8888;
-// const filePath = "./resources/test.jpeg";
-
-// app.use(fileUpload());
-// app.use(express.json());
-// app.use(
-//   cors({
-//     origin: "*",
-//   })
-// );
-
-// const credential = JSON.parse(
-//   Buffer.from(process.env.GOOGLE_SERVICE_KEY, "base64").toString()
-// );
-
-// app.get("/", (req, res) => {
-//   res.status(200).json({
-//     msg: "successful get request",
-//   });
-// });
-
-// app.post("/detectLabels", async (req, res) => {
-//   console.log("request received : ", req.files);
-//   try {
-//     if (!req.files || !req.files.image) {
-//       return res.status(400).json({ error: "Image file is missing" });
-//     }
-
-//     const imageFile = req.files.image;
-//     const imageData = imageFile.data;
-
-//     fs.writeFileSync(filePath, imageData);
-
-//     const output = await labelAPI(filePath);
-//     if (output.result == "Helmet detected") {
-//       return res
-//         .status(200)
-//         .json({ message: "Helmet detected", labels: output.allLabels });
-//     } else {
-//       return res
-//         .status(200)
-//         .json({ message: "Helmet not detected", labels: output.allLabels });
-//     }
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-// async function labelAPI(imagePath) {
-//   // const client = new ImageAnnotatorClient({
-//   //   keyFilename:
-//   //     "/Users/yash/Projects/helmetdetection-420512-658d8e9b61aa.json",
-//   // });
-
-//   const client = new ImageAnnotatorClient({
-//     projectId: "helmetdetection-420512",
-//     // important
-//     credentials: {
-//       client_email: credential.client_email,
-//       private_key: credential.private_key,
-//     },
-//   });
-
-//   const [labelResult] = await client.labelDetection(imagePath);
-//   const [objectResult] = await client.objectLocalization(imagePath);
-
-//   const labelLabels = labelResult.labelAnnotations.map(
-//     (label) => label.description
-//   );
-//   const objectLabels = objectResult.localizedObjectAnnotations.map(
-//     (obj) => obj.name
-//   );
-
-//   const allLabels = labelLabels.concat(objectLabels);
-
-//   if (allLabels.includes("Helmet")) {
-//     // console.log("Helmet detected");
-//     return { result: "Helmet detected", allLabels };
-//   } else {
-//     // console.log("Helmet not detected");
-//     return { result: "Helmet not detected", allLabels };
-//   }
-// }
-
-// app.listen(port, () => {
-//   console.log(`Server is listening at port ${port}`);
-// });
-
-// // http://127.0.0.1:8888
-
-var fs = require("fs");
-const http = require("http");
-const server = http.createServer();
-const filePath = "./resources/test.jpeg";
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const { ImageAnnotatorClient } = require("@google-cloud/vision");
+const cors = require("cors");
 require("dotenv").config();
 
-server.on("request", (request, response) => {
-  if (request.method == "POST" && request.url === "/detectLabels") {
-    var ImageFile = fs.createWriteStream(filePath, { encoding: "utf8" });
-    request.on("data", function (data) {
-      ImageFile.write(data);
-    });
+const app = express();
+const port = 8888;
+const filePath = "./resources/test.jpeg";
 
-    request.on("end", async function () {
-      ImageFile.end();
-      const labels = await labelAPI();
-      response.writeHead(200, { "Content-Type": "application/json" });
-      response.end(JSON.stringify(labels));
-    });
-  } else {
-    console.log("error");
-    response.writeHead(405, { "Content-Type": "text/plain" });
-    response.end();
-  }
-});
+app.use(fileUpload());
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const credential = JSON.parse(
   Buffer.from(process.env.GOOGLE_SERVICE_KEY, "base64").toString()
 );
 
-async function labelAPI() {
-  var o = [];
-  // Imports the Google Cloud client library
-  const vision = require("@google-cloud/vision");
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "successful get request",
+  });
+});
 
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient({
+app.post("/detectLabels", async (req, res) => {
+  console.log("request received : ", req.files);
+  try {
+    if (!req.files || !req.files.image) {
+      return res.status(400).json({ error: "Image file is missing" });
+    }
+
+    const imageFile = req.files.image;
+    const imageData = imageFile.data;
+
+    fs.writeFileSync(filePath, imageData);
+
+    const output = await labelAPI(filePath);
+    if (output.result == "Helmet detected") {
+      return res
+        .status(200)
+        .json({ message: "Helmet detected", labels: output.allLabels });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "Helmet not detected", labels: output.allLabels });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+async function labelAPI(imagePath) {
+  // const client = new ImageAnnotatorClient({
+  //   keyFilename:
+  //     "/Users/yash/Projects/helmetdetection-420512-658d8e9b61aa.json",
+  // });
+
+  const client = new ImageAnnotatorClient({
     projectId: "helmetdetection-420512",
     // important
     credentials: {
@@ -142,16 +70,88 @@ async function labelAPI() {
     },
   });
 
-  // Performs label detection on the image file
-  const [result] = await client.labelDetection(filePath);
-  const labels = result.labelAnnotations;
+  const [labelResult] = await client.labelDetection(imagePath);
+  const [objectResult] = await client.objectLocalization(imagePath);
 
-  labels.forEach((label) => {
-    o.push({ description: label.description, score: label.score });
-  });
-  return o;
+  const labelLabels = labelResult.labelAnnotations.map(
+    (label) => label.description
+  );
+  const objectLabels = objectResult.localizedObjectAnnotations.map(
+    (obj) => obj.name
+  );
+
+  const allLabels = labelLabels.concat(objectLabels);
+
+  if (allLabels.includes("Helmet")) {
+    // console.log("Helmet detected");
+    return { result: "Helmet detected", allLabels };
+  } else {
+    // console.log("Helmet not detected");
+    return { result: "Helmet not detected", allLabels };
+  }
 }
 
-const port = 8888;
-server.listen(port);
-console.log(`Listening at ${port}`);
+app.listen(port, () => {
+  console.log(`Server is listening at port ${port}`);
+});
+
+// http://127.0.0.1:8888
+
+// var fs = require("fs");
+// const http = require("http");
+// const server = http.createServer();
+// const filePath = "./resources/test.jpeg";
+// require("dotenv").config();
+
+// server.on("request", (request, response) => {
+//   if (request.method == "POST" && request.url === "/detectLabels") {
+//     var ImageFile = fs.createWriteStream(filePath, { encoding: "utf8" });
+//     request.on("data", function (data) {
+//       ImageFile.write(data);
+//     });
+
+//     request.on("end", async function () {
+//       ImageFile.end();
+//       const labels = await labelAPI();
+//       response.writeHead(200, { "Content-Type": "application/json" });
+//       response.end(JSON.stringify(labels));
+//     });
+//   } else {
+//     console.log("error");
+//     response.writeHead(405, { "Content-Type": "text/plain" });
+//     response.end();
+//   }
+// });
+
+// const credential = JSON.parse(
+//   Buffer.from(process.env.GOOGLE_SERVICE_KEY, "base64").toString()
+// );
+
+// async function labelAPI() {
+//   var o = [];
+//   // Imports the Google Cloud client library
+//   const vision = require("@google-cloud/vision");
+
+//   // Creates a client
+//   const client = new vision.ImageAnnotatorClient({
+//     projectId: "helmetdetection-420512",
+//     // important
+//     credentials: {
+//       client_email: credential.client_email,
+//       private_key: credential.private_key,
+//     },
+//   });
+
+//   // Performs label detection on the image file
+//   const [result] = await client.labelDetection(filePath);
+//   const labels = result.labelAnnotations;
+
+//   labels.forEach((label) => {
+//     o.push({ description: label.description, score: label.score });
+//   });
+//   return o;
+// }
+
+// const port = 8888;
+// server.listen(port);
+// console.log(`Listening at ${port}`);
